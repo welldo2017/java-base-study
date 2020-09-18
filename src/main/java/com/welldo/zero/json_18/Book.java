8353f281cdbaa7aeecee8f814254aec20d63eb02
welldo-win <welldo2012-win@qq.com>
2020-09-18 17:06:50 +0800

    json

---
+++ src/main/java/com/welldo/zero/json_18/Book.java
@@ -0,0 +1,38 @@
+package com.welldo.zero.json_18;
+
+import java.util.Arrays;
+
+/**
+ * 注意, 如果使用jackson来序列化时, 属性不能时private
+ * @author welldo
+ * @date 2020/9/18
+ */
+class Book {
+    public int id;
+    public String name;
+    public Author author;
+    public String isbn;
+    public String[] tags;
+
+    public Book() {
+    }
+
+    public Book(int id, String name, Author author, String isbn, String[] tags) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.isbn = isbn;
+        this.tags = tags;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author=" + author +
+                ", isbn='" + isbn + '\'' +
+                ", tags=" + Arrays.toString(tags) +
+                '}';
+    }
+}
