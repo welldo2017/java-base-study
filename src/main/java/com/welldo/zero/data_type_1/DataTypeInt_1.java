50d1a525893adce58b3212be863abb6c0e02568e
welldo-win <welldo2012-win@qq.com>
2020-09-21 11:19:47 +0800

    fix

--- src/main/java/com/welldo/zero/data_type_1/DataTypeInt_1.java
+++ src/main/java/com/welldo/zero/data_type_1/DataTypeInt_1.java
@@ -1,13 +1,25 @@
 package com.welldo.zero.data_type_1;
 
+import com.welldo.zero.oop_4.core_class.PackageType4;
+
 /**
- * 对于整型类型，Java只定义了带符号的整型，因此，最高位的bit表示符号位（0表示正数，1表示负数）。
+ * 对于整型类型，Java只定义了带符号的整型，并没有无符号整型（Unsigned）,因此，最高位的bit表示符号位（0表示正数，1表示负数）。
  * 各种整型能表示的最大范围如下：
  * byte：-128 ~ 127
  * short: -32768 ~ 32767
  * int: -2147483648 ~ 2147483647
  * long: -9223372036854775808 ~ 9223372036854775807
  *
+ * 0.
+ * 从上一节我们知道, 一个字节就是一个8位二进制数，即8个bit。
+ * 它的二进制表示范围从00000000~11111111，换算成十进制是0~255，
+ * byte是有符号整型，范围是-128~+127，但如果把byte看作无符号整型，它的范围就是0~255。
+ *
+ * 1.
+ * 而C语言则提供了CPU支持的全部数据类型，包括无符号整型。
+ * 无符号整型和有符号整型的转换在Java中就需要借助包装类型的静态方法完成。请参考{@link PackageType4} 第6点
+ *
+ * 2. 思考题: 为什么 Java 的integer “1000==1000”为false，而”100==100“为true？
  *
  * @author welldo
  * @date 2020/8/6
@@ -31,6 +43,27 @@ public class DataTypeInt_1 {
 
         System.out.println(i2);
         System.out.println(i2 == i4);
+
+        test1();
+    }
+
+    /**
+     * 为什么 Java 的integer “1000==1000”为false，而”100==100“为true？
+     * 因为自动装箱过程中, 调用了Integer 的valueOf(int i) 方法,参考{@link PackageType4}
+     *      public static Integer valueOf(int i) {...}
+     * 1. 如果值的范围在-128到127之间，此方法走integer的内部类IntegerCache.java的分支，
+     *      此内部类缓存了从-128到127之间的所有的整数对象。它就从高速返回已经缓存的实例。
+     * 2. 而其余的值,则走new 分支
+     *
+     * 为什么这里需要缓存？
+     * 合乎逻辑的理由是，“小”整数使用率比大整数要高，因此，缓存起来，可以减少潜在的内存占用。
+     */
+    static void test1(){
+        Integer a = 1000, b = 1000;
+        Integer c = 100, d = 100;
+
+        System.out.println(a == b);//idea 会友情提示:Condition 'c == d' is always 'false'
+        System.out.println(c == d);//idea 会友情提示:Condition 'c == d' is always 'true'
     }
 
 }
--- src/main/java/com/welldo/zero/oop_4/core_class/PackageType4.java
+++ src/main/java/com/welldo/zero/oop_4/core_class/PackageType4.java
@@ -1,11 +1,110 @@
 package com.welldo.zero.oop_4.core_class;
 
+import com.welldo.zero.data_type_1.DataTypeInt_1;
+
 /**
  * 包装类型
  *
+ * 0.我们已经知道，Java的数据类型分两种：
+ *  基本类型：byte，short，int，long，boolean，float，double，char
+ *  引用类型：所有class和interface,数组,枚举
+ *
+ * 0.因为包装类型非常有用，Java核心库为每种基本类型都提供了对应的包装类型：
+ *  |--------------------------------
+ *  |基本类型	    对应的引用类型
+ *  |--------------------------------
+ *  |boolean	java.lang.Boolean
+ *  |byte	    java.lang.Byte
+ *  |short	    java.lang.Short
+ *  |int	    java.lang.Integer
+ *  |long	    java.lang.Long
+ *  |float	    java.lang.Float
+ *  |double	    java.lang.Double
+ *  |char	    java.lang.Character
+ *  |--------------------------------
+ *
+ * 1. 没有自动装箱的写法,看代码
+ *
+ *
+ * 2.Auto Boxing
+ * Java编译器可以帮助我们自动在int和Integer之间转型：
+ * 这种直接把int变为Integer的赋值写法，称为自动装箱（Auto Boxing），反过来，把Integer变为int的赋值写法，称为自动拆箱（Auto Unboxing）。
+ * (注意：自动装箱和自动拆箱只发生在编译阶段,JDK>=1.5，目的是为了少写代码。)
+ * 装箱和拆箱会影响代码的执行效率，并且，自动拆箱执行时可能会报NullPointerException：
+ *
+ * 3.不变类
+ * 所有的包装类型都是不变类。我们查看Integer的源码可知，它的核心代码如下：
+ *  public final class Integer {
+ *      private final int value;    //因此，一旦创建了Integer对象，该对象就是不变的。
+ *  }
+ *  对两个Integer实例进行比较要特别注意：绝对不能用==比较，因为Integer是引用类型，必须使用equals()比较
+ *  查看{@link DataTypeInt_1}的思考题
+ *
+ * 4.进制转换
+ * Integer类本身还提供了大量方法
+ *
+ * 5.所有的整数和浮点数的包装类型都继承自Number，因此，可以非常方便地直接通过包装类型获取各种基本类型：
+ *
+ * 6.无符号整型和有符号整型的转换在Java中就需要借助包装类型的静态方法完成。
  *
  * @author welldo
  * @date 2020/8/10
  */
 public class PackageType4 {
+    public static void main(String[] args) {
+        // test1();
+        // test2();
+        // test4();
+        test5();
+    }
+
+
+    //6.因为byte的-1的二进制表示是11111111，以无符号整型转换后的int就是255
+    static void  test6(){
+        byte x = -1;
+        byte y = 127;
+        System.out.println(Byte.toUnsignedInt(x)); // 255
+        System.out.println(Byte.toUnsignedInt(y)); // 127
+
+    }
+
+    static void  test5(){
+        // 向上转型为Number:
+        Number num = new Integer(999);
+        // 获取byte, int, long, float, double:
+        byte b = num.byteValue();
+        int n = num.intValue();
+        long ln = num.longValue();
+        float f = num.floatValue();
+        double d = num.doubleValue();
+        System.out.println(b);
+    }
+
+
+    static void  test4(){
+        int x1 = Integer.parseInt("100"); // 100
+        int x2 = Integer.parseInt("100", 16); // 256,因为按16进制解析
+        System.out.println(x1);
+        System.out.println(x2);
+    }
+
+    //2.
+    static void  test2(){
+        Integer n = 1000; //装箱 编译器自动使用Integer.valueOf(int)
+        int x = n;      //拆箱 编译器自动使用Integer.intValue()
+        n =1001;
+
+        // Integer n1 = null;//空指针异常
+        // int i = n1;
+    }
+
+    //1.手动装箱
+    static void  test1(){
+        int i = 100;
+        Integer n1 = new Integer(i);// 通过new操作符创建Integer实例(不推荐使用,会有编译警告):
+
+        Integer n2 = Integer.valueOf(i);// 通过静态方法valueOf(int)创建Integer实例:
+        Integer n3 = Integer.valueOf("100");// 通过静态方法valueOf(String)创建Integer实例:
+        System.out.println(n3.intValue());
+    }
 }
